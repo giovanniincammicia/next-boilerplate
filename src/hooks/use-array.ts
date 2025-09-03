4514ae9ab8dfc9b05091c33d7d9da2e87b60de22
Giovanni Incammicia <giovanni.incammicia@gmail.com>
2025-09-03 19:28:09 +0000

    fix: build error

--- src/hooks/use-array.ts
+++ src/hooks/use-array.ts
@@ -1,10 +1,12 @@
+import { useReducer } from "react";
+import { isNullish } from "@/lib/utils";
+
 /**
  * Custom React hook for managing an array state with toggle functionality.
  *
- * @template T - The type of elements in the array.
- * @param {T[]} [defaultValue=[]] - The initial value of the array.
- * @param {(a: T, b: T) => boolean} [equal=(a, b) => a === b] - Function to determine equality between array elements.
- * @returns {[T[], (v: T) => void]} - Returns the current array and a dispatch function to toggle elements.
+ * @param defaultValue - The initial value of the array.
+ * @param equal - Function to determine equality between array elements.
+ * @returns - Returns the current array and a dispatch function to toggle elements.
  *
  * @remarks
  * - When the dispatch function is called with a value:
@@ -19,9 +21,6 @@
  * toggleItem(3); // Adds 3 to the array
  * ```
  */
-import { useReducer } from "react";
-import { isNullish } from "@/lib/utils";
-
 export function useArray<T>(defaultValue: T[] = [], equal = (a: T, b: T) => a === b) {
 	return useReducer<T[], [T]>((s, v) => {
 		if (isNullish(v)) return s;
