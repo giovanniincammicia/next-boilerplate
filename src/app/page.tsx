ba8ddad67a93113a47f0220e8528a2f7f4346027
Giovanni Incammicia <giovanni.incammicia@gmail.com>
2025-09-03 18:51:42 +0000

    fix: build errors

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,8 @@
 import { db } from "@/db";
 import { TABLE_user } from "@/db/schema/auth";
 
+export const dynamic = "force-dynamic";
+
 export default async function Home() {
 	const userData = await db.select().from(TABLE_user);
 	return <main>Hello world - {userData.length} users.</main>;
